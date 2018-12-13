860ac29c3264339600c0e8c636b3203243e1d719
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-12 20:24:14 -0500

    saved actually

--- scripts/bookmarkList.js
+++ scripts/bookmarkList.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const bookmarkList = (function(){
+
+  function generateFormNavigation() {
+    // if the newsubmitform  is true, then  generate the submit form
+    // cancel also toggles the newsubmitform button on the submit form
+    // otherwise generate the button that toggles the form along with the ratings filter
+
+  }
+  
   function generateItemElement(){
   // if item expanded is true, desplay expanded html
   // the error message display also happens here
@@ -19,6 +27,26 @@ const bookmarkList = (function(){
     //  insert into dom
 
   }
+
+  function getItemIdFromElement(){
+    // id is stored in the element, for store access
+
+  }
+
+  function handleNewSubmitToggle(){
+
+  }
+
+  function handleNewItemSubmit(){
+    // accesss the post request
+  }
+
+  function handleDeleteButtonClicked(){
+    // api delete request
+  }
+
+  function 
+
   
   function bindEventListeners(){
 
